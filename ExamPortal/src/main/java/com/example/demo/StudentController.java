ec60cb1d0f095393de14bc84e93567c62314e2a0
Admin <Admin@DESKTOP-1TVRNL4>
2025-06-14 20:07:43 +0530

    14-6-25

--- ExamPortal/src/main/java/com/example/demo/StudentController.java
+++ ExamPortal/src/main/java/com/example/demo/StudentController.java
@@ -34,7 +34,7 @@ public class StudentController {
 	public AnswerRepository answerepository;
 	
 	@Autowired
-	public JavaRepository javarepository;
+    public JavaRepository javarepository;
 	
 	@Autowired
 	public ScheduleRepository schedulerepository;
@@ -136,43 +136,35 @@ public class StudentController {
 	}
 	
 	//Add  questions(admin page)
-	@GetMapping("add")
-	public String question(Model model) {
-		model.addAttribute("question", new AddQuestion());
-		return "addquestion";
-	}
-	
-	@PostMapping("save")
-	public String save(@ModelAttribute Java question,Model model) {
-		System.out.println();
-		if(question.getSubject().equals("Java")) {
-			javarepository.save(question);
-			return "success";
-		}
-		else {
-			
-			return "success";
-		}
-		
-	}
+	/*
+	 * @GetMapping("add") public String question(Model model) {
+	 * model.addAttribute("question", new AddQuestion()); return "addquestion"; }
+	 * 
+	 * @PostMapping("save") public String save(@ModelAttribute Java question,Model
+	 * model) { System.out.println(); if(question.getSubject().equals("Java")) {
+	 * javarepository.save(question); return "success"; } else {
+	 * 
+	 * return "success"; }
+	 * 
+	 * }
+	 */
 	
 	//Common question bank
-	@GetMapping("add/{id}")
-	public String addquestion(@PathVariable  Long id,Model model) {
-		Java q1=javarepository.findById(id).orElse(null);
-		//Java q12=q1;
-		AddQuestion que=new AddQuestion(q1.qid,q1.question,q1.opt1,q1.opt2,q1.opt3,q1.opt4,q1.ans,q1.subject,q1.batch,q1.level);
-		System.out.println(que);
-		
-		questionrepository.save(que);
-		return "select";
-	}
-	@GetMapping("select1")
-	public String select(@ModelAttribute Java question,Model model) {
-		List<Java> ques=javarepository.findAll();
-		model.addAttribute("questions",ques);
-		return "select";
-	}
+	//@GetMapping("add/{id}")
+	/*
+	 * public String addquestion(@PathVariable Long id,Model model) { Java
+	 * q1=javarepository.findById(id).orElse(null); //Java q12=q1; AddQuestion
+	 * que=new
+	 * AddQuestion(q1.qid,q1.question,q1.opt1,q1.opt2,q1.opt3,q1.opt4,q1.ans,q1.
+	 * subject,q1.batch,q1.level); System.out.println(que);
+	 * 
+	 * questionrepository.save(que); return "select"; }
+	 */
+	/*
+	 * @GetMapping("select1") public String select(@ModelAttribute Java
+	 * question,Model model) { List<Java> ques=javarepository.findAll();
+	 * model.addAttribute("questions",ques); return "select"; }
+	 */
 	
 	//Displaying question and option
 	@RequestMapping("alltests/view/{id}")
--- ExamPortal/src/main/java/com/example/demo/TeacherContoller.java
+++ ExamPortal/src/main/java/com/example/demo/TeacherContoller.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +19,13 @@ public class TeacherContoller {
 	
 	@Autowired
 	public ScheduleRepository schedulerepository;
+	
+	@Autowired
+	public JavaRepository javarepository;
+	
+	@Autowired
+	public QuestionRepository questionrepository;
+	
 
 	@RequestMapping("/trainer")
 	public String tainerRegistration(Model model) {
@@ -75,6 +85,46 @@ public class TeacherContoller {
 		return "success";
 	}
 	
+	
+	//Add  questions(admin page)
+		@GetMapping("add")
+		public String question(Model model) {
+			model.addAttribute("question", new AddQuestion());
+			return "addquestion";
+		}
+		
+		@PostMapping("save")
+		public String save(@ModelAttribute Java question,Model model) {
+			System.out.println();
+			if(question.getSubject().equals("Java")) {
+				javarepository.save(question);
+				return "success";
+			}
+			else {
+				
+				return "success";
+			}
+			
+		}
+	
+		//Common question bank
+		@GetMapping("add/{id}")
+		public String addquestion(@PathVariable  Long id,Model model) {
+			Java q1=javarepository.findById(id).orElse(null);
+			//Java q12=q1;
+			AddQuestion que=new AddQuestion(q1.qid,q1.question,q1.opt1,q1.opt2,q1.opt3,q1.opt4,q1.ans,q1.subject,q1.batch,q1.level);
+			System.out.println(que);
+			
+			questionrepository.save(que);
+			return "select";
+		}
+	
+		@GetMapping("select1")
+		public String select(@ModelAttribute Java question,Model model) {
+			List<Java> ques=javarepository.findAll();
+			model.addAttribute("questions",ques);
+			return "select";
+		}
 	/*
 	 * @RequestMapping("") public String scheduleTest() { return"schedule"; }
 	 */
